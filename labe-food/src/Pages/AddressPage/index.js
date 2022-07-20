989ef472fa0a8b7fca0a0aa61e261a549274db25
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-07-19 23:00:57 -0300

    Páginas de cadastro

--- labe-food/src/Pages/AddressPage/index.js
+++ labe-food/src/Pages/AddressPage/index.js
@@ -1,12 +1,85 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { LoginBox } from './styled'
+import Logo from '../../assets/logo-laranja.svg'
 
 const AddressPage = () => {
   const navigate = useNavigate()
   return (
-    <div>
-      <button onClick={() => navigate('/home')}>Login</button>
-    </div>
+    <LoginBox>
+      <p onClick={() => navigate('/')}>←</p>
+      <p>Meu Endereço</p>
+      <div>
+        <TextField
+          fullWidth
+          margin={'normal'}
+          required
+          id="endereco"
+          placeholder="Rua/Av."
+          label="Logradouro"
+        />
+        <TextField
+          fullWidth
+          autoFocus
+          margin={'normal'}
+          required
+          id="numero"
+          label="Número"
+          placeholder="Número"
+        />
+        <TextField
+          fullWidth
+          autoFocus
+          margin={'normal'}
+          required
+          id="complemento"
+          label="Complemento"
+          placeholder="Apto./Bloco"
+        />
+        <TextField
+          fullWidth
+          autoFocus
+          margin={'normal'}
+          required
+          id="bairro"
+          label="Bairro"
+          placeholder="Bairro"
+        />
+        <TextField
+          fullWidth
+          autoFocus
+          margin={'normal'}
+          required
+          id="cidade"
+          label="Cidade"
+          placeholder="Cidade"
+        />
+        <TextField
+          fullWidth
+          autoFocus
+          margin={'normal'}
+          required
+          id="estado"
+          label="Estado"
+          placeholder="Estado"
+        />
+
+      </div>
+      <div>
+        <Button
+          variant="contained"
+          type={"submit"}
+          fullWidth
+          color={"primary"}
+          margin={'normal'}
+          onSubmit={() => navigate('/cadastro-endereco')}
+        >
+          Salvar
+        </Button>
+      </div>
+    </LoginBox>
   )
 }
 
--- labe-food/src/Pages/ProfilePage/index.js
+++ labe-food/src/Pages/ProfilePage/index.js
@@ -1,49 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { LoginBox } from './styled'
+
 
 const ProfilePage = () => {
   const navigate = useNavigate()
   return (
-    <LoginBox>
-            <p>Entrar</p>
-      <div>
-      <TextField
-        fullWidth
-        autoFocus
-        margin={'normal'}
-        required
-        id="outlined-required"
-        label="E-mail"
-        placeholder="email@email.com"
-      />
-      <TextField
-        fullWidth
-        autoFocus
-        margin={'normal'}
-        required
-        id="outlined-disabled"
-        label="Senha"
-        placeholder="Senha"
-        type="password"
-      />
-      </div>
+    <div>
 
-      <Button
-        variant="contained"
-        type={"submit"}
-        fullWidth
-        color={"primary"}
-        margin={'normal'}
-      >
-        Entrar
-      </Button>
       <button onClick={() => navigate('/')}>Logout</button>
 
       <button onClick={() => navigate('/home')}>Voltar pra Home</button>
-    </LoginBox>
+    </div>
   )
 }
 
