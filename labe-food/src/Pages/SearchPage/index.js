4408953e5cffb6310deeb0e5a793399be2d39c78
camila donda <camiladonda@gmail.com>
2022-07-28 21:20:29 +1000

    SearchPage updated

--- labe-food/src/Pages/SearchPage/index.js
+++ labe-food/src/Pages/SearchPage/index.js
@@ -1,76 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Search from '../../Components/Search.js'
-import axios from 'axios'
-import CardRestaurant from '../../Components/CardRestaurant'
-import styled from 'styled-components'
-import Header from '../../Components/Header.js'
-import { BASE_URL } from '../../constants/url.js'
+import styled from 'styled-components';
+import { BASE_URL } from '../../constants/url.js';
+import CardRestaurant from '../../Components/CardRestaurant.js';
+import { useGetRest } from '../../hooks/useGetRest.js';
+import { useState } from 'react';
 
 const StyledBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`
-const SearchPage = () => {
-  const [loading, setLoading] = useState(false)
-  const [posts, setPosts] = useState([])
-  const [ setSearchTitle] = useState('')
+`;
 
-  useEffect(() => {
-    const loadPost = async () => {
-      setLoading(true)
-      await axios
-        .get(`${BASE_URL}/restaurants`, {
-          headers: {
-            auth: localStorage.getItem('token')
-          }
-        })
-        .then(response => {
-          setPosts(response.data.restaurants)
-          console.log(response.data.restaurants)
-          setLoading(false)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
-    loadPost()
-  }, [])
+const SearchPage = () => {
+  const [infoRestaurante] = useGetRest(`${BASE_URL}/restaurants`);
+  const [busca, setBusca] = useState('');
+  const [select, setSelect] = useState('');
+
+  //console.log(infoRestaurante);
+
+  //filtrando por categoria
+  const restauranteCategoria = infoRestaurante.filter((item) =>
+    item.category.toLowerCase().includes(select.toLocaleLowerCase()),
+  );
+  //filtrando por restaurante
+  const restauranteFiltrado = restauranteCategoria.filter((i) =>
+    i.name.toLowerCase().includes(busca.toLocaleLowerCase()),
+  );
 
   return (
-    <>
-      <Header />
-      <StyledBox>
-        <br />
-        <Search onChange={e => setSearchTitle(e.target.value)} />
+    <StyledBox>
+      <input value={busca} onChange={(e) => setBusca(e.target.value)} />
+      <select value={select} onChange={(e) => setSelect(e.target.value)}>
+        <option>Categoria</option>
+        {infoRestaurante.map((info) => (
+          <option>{info.category}</option>
+        ))}
+      </select>
 
-        <br />
-        <h3>Busque por nome de restaurante</h3>
-        {loading ? (
-          <h4>Carregando ... </h4>
-        ) : (
-          posts
-            // .filter((value) => {
-            //   if (searchTitle === '') {
-            //     return value;
-            //   } else if (
-            //     value.title.toLowerCase().includes(searchTitle.toLowerCase())
-            //   ) {
-            //     return value;
-            //   }
-            // })
-            .map(item => (
-              <CardRestaurant key={item.id}>
-                {item.logoUrl}
-                {item.name} {item.shipping}
-                {item.deliveryTime}
-              </CardRestaurant>
-            ))
-        )}
-      </StyledBox>
-    </>
-  )
-}
-
-export default SearchPage
+      {restauranteFiltrado.map((item) => (
+        <CardRestaurant
+          key={item.id}
+          id={item.id}
+          logoUrl={item.logoUrl}
+          name={item.name}
+          shipping={item.shipping}
+          deliveryTime={item.deliveryTime}
+        />
+      ))}
+    </StyledBox>
+  );
+};
+export default SearchPage;
