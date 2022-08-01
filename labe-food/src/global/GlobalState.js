27ba4897a444e71d6730bb62f24ee5621e356c5c
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-08-01 20:35:51 -0300

    tentei fazer o carrinho, mudei as fórmulas de lugar

--- labe-food/src/global/GlobalState.js
+++ labe-food/src/global/GlobalState.js
@@ -2,7 +2,6 @@ import { InsertEmoticon } from '@mui/icons-material'
 import React, { useState } from 'react'
 import { GlobalStateContext } from './GlobalStateContatex'
 
-
 const GlobalState = props => {
   const [restaurants, setRestaurants] = useState([])
   const [restaurantDetail, setRestaurantDetail] = useState({})
@@ -11,34 +10,22 @@ const GlobalState = props => {
   const [address, setAddress] = useState({})
   const [activeOrder, setActiveOrder] = useState({})
   const [orderHistory, setOrderHistory] = useState([])
-  
+
   const [cart, setCart] = useState([])
   const [infoRestaurant, setInfoRestaurant] = useState({})
   const [subTotal, setSubtotal] = useState(0)
 
   const addProduct = (product, restaurant, quantidade) => {
-    const newProduct = {...product, quantity:quantidade}
+    const newProduct = { ...product, quantity: quantidade }
     const newCart = [...cart, newProduct]
     setCart(newCart)
 
     const newInfoRestaurant = restaurant
     setInfoRestaurant(newInfoRestaurant)
 
-    const newSubTotal = subTotal + (product.price*quantidade)
+    const newSubTotal = subTotal + product.price * quantidade
     setSubtotal(newSubTotal)
   }
-
-  const removeProduct = (product) => {
-    const newCart = cart.filter((item) =>{
-      return InsertEmoticon.id !== product.id
-    })
-    setCart(newCart)
-
-    const newSubTotal = subTotal - (product.price*product.quantity)
-    setSubtotal(newSubTotal)
-  }
-
-  const functions = {addProduct, removeProduct}
 
   const states = {
     restaurants,
@@ -67,7 +54,7 @@ const GlobalState = props => {
   }
 
   return (
-    <GlobalStateContext.Provider value={{ setters, states, functions }}>
+    <GlobalStateContext.Provider value={{ setters, states }}>
       {props.children}
     </GlobalStateContext.Provider>
   )
